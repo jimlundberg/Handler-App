ef15f1ea8f5fdb2b315e07ba8ecfd68b1dd1d558
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-09-18 13:01:51 -0700

    Added <CR>'s

--- Status.Services/InputJobsScanThread.cs
+++ Status.Services/InputJobsScanThread.cs
@@ -102,7 +102,7 @@ namespace Status.Services
                     string directory = dirInfo.ToString();
                     string job = directory.ToString().Replace(StaticClass.IniData.InputDir, "").Remove(0, 1);
 
-                    StaticClass.Log(string.Format("Starting Input Job {0} at {1:HH:mm:ss.fff}", directory, DateTime.Now));
+                    StaticClass.Log(string.Format("\nStarting Input Job {0} at {1:HH:mm:ss.fff}", directory, DateTime.Now));
 
                     // Remove job run from Input Job directory list
                     lock (RemoveLock)
@@ -207,7 +207,7 @@ namespace Status.Services
                 {
                     string directory = StaticClass.IniData.InputDir + @"\" + job;
 
-                    StaticClass.Log(string.Format("Starting Input Job {0} at {1:HH:mm:ss.fff}", directory, DateTime.Now));
+                    StaticClass.Log(string.Format("\nStarting Input Job {0} at {1:HH:mm:ss.fff}", directory, DateTime.Now));
 
                     // Reset Input job file scan flag
                     StaticClass.InputFileScanComplete[job] = false;
